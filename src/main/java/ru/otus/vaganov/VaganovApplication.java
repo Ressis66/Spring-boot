610cdaa5744d03110898d83caf95c3c490438665
Ressis66 <vaganov.aleksandr.88@mail.ru>
2022-06-28 10:57:29 +0500

    Fifth try

---
+++ src/main/java/ru/otus/vaganov/VaganovApplication.java
@@ -0,0 +1,34 @@
+package ru.otus.vaganov;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.otus.vaganov.domain.Question;
+import ru.otus.vaganov.domain.User;
+import ru.otus.vaganov.service.QuestionService;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+
+@SpringBootApplication
+public class VaganovApplication implements CommandLineRunner {
+
+	@Autowired
+ 	QuestionService service;
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaganovApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		 service.initTestingService();
+
+	}
+}
+
+
